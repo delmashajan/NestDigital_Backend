3740fe712a5f6aca4a8b01bce7e2dd1bb99ab38b
delmashajan <delmashajan@gmail.com>
2022-12-12 12:52:45 +0530

    search employee

--- src/main/java/com/example/NestDigital_Baackend/dao/EmployeeDao.java
+++ src/main/java/com/example/NestDigital_Baackend/dao/EmployeeDao.java
@@ -1,7 +1,14 @@
 package com.example.NestDigital_Baackend.dao;
 
 import com.example.NestDigital_Baackend.model.Employee;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface EmployeeDao extends CrudRepository<Employee,Integer> {
+
+    @Query(value = "SELECT `id`, `designation`, `email`, `empcode`, `name`, `password`, `phone`, `salary`, `username` FROM `employee` WHERE `empcode`= :empcode",nativeQuery = true)
+    List<Employee> Searchemployee (@Param("empcode") Integer empcode);
 }
