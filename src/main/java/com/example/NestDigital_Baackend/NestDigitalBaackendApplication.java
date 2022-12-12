866d2820b22540e280b730667c8afb047fc96e26
delmashajan <delmashajan@gmail.com>
2022-12-12 11:45:09 +0530

    initial

---
+++ src/main/java/com/example/NestDigital_Baackend/NestDigitalBaackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.NestDigital_Baackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NestDigitalBaackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NestDigitalBaackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/NestDigital_Baackend/NestDigitalBaackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.NestDigital_Baackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NestDigitalBaackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
